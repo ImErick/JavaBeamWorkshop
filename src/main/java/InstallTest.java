02327ce6eb3fd6c094741f7da4b6158283de18de
ImErick <3r1ck.442@gmail.com>
2020-10-25 00:49:26 -0500

    add a PipelineOptions example in InstallTest class in order to pass parameters to de Pipeline process instead of hardcore the paths of the input, output and extension

--- src/main/java/InstallTest.java
+++ src/main/java/InstallTest.java
@@ -1,19 +1,21 @@
 import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.io.TextIO;
+import org.apache.beam.sdk.options.PipelineOptionsFactory;
 import org.apache.beam.sdk.values.PCollection;
 
 public class InstallTest {
     public static void main(String[] args) {
-        Pipeline pipeline = Pipeline.create();
+        MyOptions options = PipelineOptionsFactory.fromArgs(args).withValidation().as(MyOptions.class);
+        Pipeline pipeline = Pipeline.create(options);
         // de esta manera se crea un DF desde un archivo CSV
-        PCollection<String> output =  pipeline.apply(TextIO.read().from("/Users/erickgarcia/Downloads/addresses.csv"));
+        PCollection<String> output =  pipeline.apply(TextIO.read().from(options.getInputFile()));
 
         // de esta manera se escribe el DF en un path del sistema
         output.apply(TextIO
                 .write()
-                .to("/Users/erickgarcia/Downloads/test.csv")
+                .to(options.getOutputFile())
                 .withNumShards(1) // si no le agregamos esta opcion lo particiona por supongo numero de cores
-                .withSuffix(".csv")); // con esto le agrego el formato al archivo de salida
+                .withSuffix(options.getExtension())); // con esto le agrego el formato al archivo de salida
 
         pipeline.run(); // es importante esta parte porque si no, no corre nara
         System.out.println("done!");
---
+++ src/main/java/MyOptions.java
@@ -0,0 +1,12 @@
+import org.apache.beam.sdk.options.PipelineOptions;
+
+public interface MyOptions extends PipelineOptions {
+    void setInputFile(String file);
+    String getInputFile();
+
+    void setOutputFile(String file);
+    String getOutputFile();
+
+    void setExtension(String extension);
+    String getExtension();
+}
