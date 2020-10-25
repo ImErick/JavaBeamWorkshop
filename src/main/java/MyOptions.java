1545835bf479b040cba11852648c6a1e8d60f9db
ImErick <3r1ck.442@gmail.com>
2020-10-25 00:53:21 -0500

    add java command example in order to run our jar

--- src/main/java/MyOptions.java
+++ src/main/java/MyOptions.java
@@ -10,3 +10,12 @@ public interface MyOptions extends PipelineOptions {
     void setExtension(String extension);
     String getExtension();
 }
+
+/*
+ * Ejemplo del comando de Java para la consola y poder correr el jar parametrizado:
+ *
+ * java -cp JavaBeamWorkshop-1.0-SNAPSHOT-jar-with-dependencies.jar InstallTest
+ * --inputFile="/Users/erickgarcia/Downloads/addresses.csv"
+ * --outputFile="/Users/erickgarcia/Desktop/test.csv"
+ * --extension=".csv"
+ */
